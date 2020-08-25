1b88c3ed1f43265d9a0a355132f059cd95f48f78
nimaester <nimaester@gmail.com>
2020-08-25 16:13:25 -0700

    final update

--- client/src/Components/ModalRatingsBar.jsx
+++ client/src/Components/ModalRatingsBar.jsx
@@ -2,12 +2,29 @@ import React from "react";
 import styles from "../Styles/ModalRatingsBar.css";
 
 const ModalRatingsBar = (props) => {
+  let middle = {
+    marginTop: "10px",
+    float: "left",
+    width: "200px",
+  };
 
+  let barBackground = {
+    width: "100px",
+    backgroundColor: "#cccccc",
+    textAlign: "center",
+    color: "white",
+  };
+
+  let ratingBar = {
+    width: `${(props.rating / 5) * 100}%`,
+    height: "4px",
+    backgroundColor: "black",
+  };
   return (
     <div className={styles.ratings}>
-      <div className={styles.middle}>
-        <div className={styles.barBackground}>
-          <div className={styles.ratingBar}></div>
+      <div style={middle}>
+        <div style={barBackground}>
+          <div style={ratingBar}></div>
         </div>
       </div>
       <span className={styles.ratingNumber}>{props.rating}</span>
