83327401ba32b161fad24e752f2ddb81c41aa544
Erfanch <erfan.chinif@gmail.com>
2020-09-07 19:15:10 -0700

    Bash commands for creating/importing files finished

--- SDC/PostGres/seed.sh
+++ SDC/PostGres/seed.sh
@@ -1,9 +1,46 @@
+# **************************GENERATE DATA**************************
+numOfhouseFiles=10;
+numOfUserFiles=1;
+numOfReviewFiles=30;
+numOfcommentFiles=15;
+
+for ((i = 0 ; i < ${numOfhouseFiles} ; i++)); do
+  echo "Houses File: $i"
+  node postGresSeed.js $i "writeTenMillionHouses"
+done
+
+for ((i = 0 ; i < ${numOfUserFiles} ; i++)); do
+  echo "Users File: $i"
+  node postGresSeed.js $i "writeOneMillionUsers"
+done
+
+for ((i = 0 ; i < ${numOfReviewFiles} ; i++)); do
+  echo "Reviews File: $i"
+  node postGresSeed.js $i "write150MillionReviews"
+done
+
+for ((i = 0 ; i < ${numOfcommentFiles} ; i++)); do
+  echo "Comments File: $i"
+  node postGresSeed.js $i "writeFifteenMillionComments"
+done
+
+
 psql -U me -d sdc -a -f postGresSchema.sql
 
-psql -U me -d sdc -c  "COPY houses(houseId, houseName) FROM '/Users/erfan/Desktop/JunBootcamp/SDC/reviews-section/SDC/PostGres/houses2.csv' DELIMITER ',' CSV HEADER;"
+# ****** HOW TO IMPORT FILES INTO TABLES CQLSH ******
 
-psql -U me -d sdc -c  "COPY users(userId, userName, userUrl) FROM '/Users/erfan/Desktop/JunBootcamp/SDC/reviews-section/SDC/PostGres/users2.csv' DELIMITER ',' CSV HEADER"
+for ((i = 0 ; i < ${numOfhouseFiles} ; i++)); do
+  psql -U me -d sdc -c "COPY houses(houseId, houseName) FROM '/Users/erfan/Desktop/JunBootcamp/SDC/reviews-section/SDC/PostGres/houses$i.csv' DELIMITER ',' CSV HEADER;"
+done
 
-psql -U me -d sdc -c "COPY reviews(reviewId, userId, review, houseId, dateReviwed, cleanRating, accRating, commRating, locationRating, checkInRating, valueRating) FROM '/Users/erfan/Desktop/JunBootcamp/SDC/reviews-section/SDC/PostGres/reviews3.csv' DELIMITER ',' CSV HEADER"
+for ((i = 0 ; i < ${numOfUserFiles} ; i++)); do
+  psql -U me -d sdc -c "COPY users(userId, userName, userUrl) FROM '/Users/erfan/Desktop/JunBootcamp/SDC/reviews-section/SDC/PostGres/users$i.csv' DELIMITER ',' CSV HEADER"
+done
 
-psql -U me -d sdc -c "COPY comments(commentId, reviewId, comment) FROM '/Users/erfan/Desktop/JunBootcamp/SDC/reviews-section/SDC/PostGres/comments2.csv' DELIMITER ',' CSV HEADER"
+for ((i = 0 ; i < ${numOfReviewFiles} ; i++)); do
+  psql -U me -d sdc -c "COPY reviews(reviewId, userId, review, houseId, dateReviwed, cleanRating, accRating, commRating, locationRating, checkInRating, valueRating) FROM '/Users/erfan/Desktop/JunBootcamp/SDC/reviews-section/SDC/PostGres/reviews${i}.csv' DELIMITER ',' CSV HEADER"
+done
+
+for ((i = 0 ; i < ${numOfcommentFiles} ; i++)); do
+  psql -U me -d sdc -c "COPY comments(commentId, reviewId, comment) FROM '/Users/erfan/Desktop/JunBootcamp/SDC/reviews-section/SDC/PostGres/comments$i.csv' DELIMITER ',' CSV HEADER"
+done
